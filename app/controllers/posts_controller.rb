875e02ef43a51d9fd63cee2d7dd5b51725909ed8
Andrew Walters <andrewjpwalters@gmail.com>
2023-03-24 02:27:38 -0500

    add update to post controller and cryptid_id and location_id to post params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,20 @@ class PostsController < ApplicationController
         post = @current_user.posts.create!(post_params)
         render json: post, status: :created
       end
+
+      def update
+		post = Post.find_by(id:params[:id])
+		if post
+			post.update(post_params)
+			render json: post, status: :accepted
+		else
+			render json: {error: “Post not found”}, status: :not_found
+		end
+	end
     
       private
     
       def post_params
-        params.permit(:comment)
+        params.permit(:comment, :cryptid_id, :location_id)
       end
 end
