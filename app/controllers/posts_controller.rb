b929576baa60901270e1568cd8cde04c36545beb
Andrew Walters <andrewjpwalters@gmail.com>
2023-03-24 01:43:38 -0500

    add index, create and params to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+    def index
+        render json: Post.all
+      end
+    
+      def create
+        post = @current_user.posts.create!(post_params)
+        render json: post, status: :created
+      end
+    
+      private
+    
+      def post_params
+        params.permit(:comment)
+      end
 end
