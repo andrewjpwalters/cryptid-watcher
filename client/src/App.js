9440236a55dd3cc3e2657b6bf863122902712f0d
Andrew Walters <andrewjpwalters@gmail.com>
2023-03-24 02:59:15 -0500

    add imports and routes and user auto login to App,js, create CryptidList and PostList components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Login from "./Login"
+import New from "./New"
+import PostList from "./PostList"
 
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
+
   return (
-    <h1>Hello!</h1>
+    <>
+      <NavBar user={user} setUser={setUser} />
+      <main>
+        <Switch>
+          <Route exact path="/new">
+            <New user={user} />
+          </Route>
+          <Route exact path="/cryptids">
+            <CryptidList />
+          </Route>
+          <Route exact path="/posts">
+            <PostList user={user} />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <h1>404 NOT FOUND</h1>
+          </Route>
+        </Switch>
+      </main>
+    </>
   );
 }
 
