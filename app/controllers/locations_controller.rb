807fed5d53f2328237d7eaa167c26ea4a1d59cb4
Andrew Walters <andrewjpwalters@gmail.com>
2023-03-24 01:42:08 -0500

    add index, create and params to location controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,16 @@
 class LocationsController < ApplicationController
+    def index
+        render json: Location.all
+      end
+    
+      def create
+        location = @current_user.locations.create!(location_params)
+        render json: location, status: :created
+      end
+    
+      private
+    
+      def location_params
+        params.permit(:name)
+      end
 end
