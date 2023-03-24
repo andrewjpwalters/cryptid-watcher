464aca2f1415cde3108187129b8d9e186197e239
Andrew Walters <andrewjpwalters@gmail.com>
2023-03-24 01:40:34 -0500

    add index, create and params to cryptid controller

--- app/controllers/cryptids_controller.rb
+++ app/controllers/cryptids_controller.rb
@@ -1,2 +1,16 @@
 class CryptidsController < ApplicationController
+    def index
+        render json: Cryptid.all
+      end
+    
+      def create
+        cryptid = @current_user.cryptids.create!(cryptid_params)
+        render json: cryptid, status: :created
+      end
+    
+      private
+    
+      def cryptid_params
+        params.permit(:name, :description, :image_url)
+      end
 end
