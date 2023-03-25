cb34b2dcc199dc2b2ff495833b4903477842fc7f
Andrew Walters <andrewjpwalters@gmail.com>
2023-03-25 04:02:56 -0500

    import CryptidForm and LocationForm to New.js

---
+++ client/src/New.js
@@ -0,0 +1,15 @@
+import CryptidForm from "./CryptidForm";
+import LocationForm from "./LocationForm";
+
+function New() {
+    return (
+        <>
+            <h1>Hello from New</h1>
+            <CryptidForm />
+            <LocationForm />
+
+        </>
+    )
+}
+
+export default New;
