3f5bc4877b91a918d0357048adcf17e7067868b9
Andrew Walters <andrewjpwalters@gmail.com>
2023-03-30 02:46:15 -0500

    fix formatting issue in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,36 +1,36 @@
 class PostsController < ApplicationController
-    def index
-        render json: Post.all
-      end
-    
-      def create
-        post = @current_user.posts.create!(post_params)
-        render json: post, status: :created
-      end
-
-      def update
-		post = Post.find_by(id:params[:id])
-		if post
-			post.update(post_params)
-			render json: post, status: :accepted
-		else
-			render json: {error: “Post not found”}, status: :not_found
-		end
-
-        def destroy
-            post = Post.find_by(id:params[:id])
-            if post
-                post.destroy
-                head :no_content
-            else
-                render json: {error: “Post not found“}, status: :not_found
-            end
-        end
+  def index
+    render json: Post.all
+  end
+  
+  def create
+    post = @current_user.posts.create!(post_params)
+    render json: post, status: :created
+  end
+  
+  def update
+    post = Post.find_by(id:params[:id])
+    if post
+      post.update(post_params)
+      render json: post, status: :accepted
+    else
+      render json: {error: “Post not found”}, status: :not_found
+    end
+  end
+  
+  def destroy
+    post = Post.find_by(id:params[:id])
+    if post
+      post.destroy
+      head :no_content
+    else
+      render json: {error: “Post not found“}, status: :not_found
+    end
 	end
-    
-      private
-    
-      def post_params
-        params.permit(:comment, :cryptid_id, :location_id)
-      end
+  
+  private
+  
+  def post_params
+    params.permit(:comment, :cryptid_id, :location_id)
+  end
 end
