21e57283f8a3ddcbb5c8ca63a07c53cce3352932
Andrew Walters <andrewjpwalters@gmail.com>
2023-04-18 15:33:24 -0500

    change locations association from attribute to has_many

--- app/serializers/cryptid_serializer.rb
+++ app/serializers/cryptid_serializer.rb
@@ -1,4 +1,6 @@
 class CryptidSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :image_url, :locations
+  attributes :id, :name, :description, :image_url
+
+  has_many :locations
   has_one :user
 end
